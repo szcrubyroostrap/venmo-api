e1c71c4066185ded674f219767d9b3da16801cd6
sebastian zapata <sebastianzapata@MacBook-Pro-de-sebastian.local>
2021-07-25 23:22:24 -0500

    creating endpoints

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  include Pagy::Backend
+  before_action :set_user
+
+  def payment
+    user_b = User.find(payment_params[:friend_id])
+    FundTransferService.new(@user, user_b, payment_params[:amount],
+                            payment_params[:description]).pay
+    head :ok
+  end
+
+  def feed
+    pagy, my_feed = pagy_array(@user.my_feed, { page: feed_params[:page] })
+    render json: { feed: my_feed }, status: :ok
+  end
+
+  def balance
+    render json: { balance: @user.amount }, status: :ok
+  end
+
+  private
+
+  def payment_params
+    params.permit(:friend_id, :amount, :description)
+  end
+
+  def feed_params
+    params.permit(:page)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
